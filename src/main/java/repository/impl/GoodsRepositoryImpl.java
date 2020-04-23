bf53d6e17ca4228cf417aa039bdcbd24374e519a
Fedor Zholud <fvzholud@gmail.com>
2020-04-23 13:00:47 +0300

    added java8

--- src/main/java/repository/impl/GoodsRepositoryImpl.java
+++ src/main/java/repository/impl/GoodsRepositoryImpl.java
@@ -14,25 +14,30 @@ public class GoodsRepositoryImpl implements GoodsRepository {
     @PersistenceContext(name = "myUnit")
     private EntityManager em;
 
+    @Override
     public void create(GoodsEntity goods) {
         em.persist(goods);
     }
 
+    @Override
     public GoodsEntity read(int id) {
         GoodsEntity goods = em.find(GoodsEntity.class, id);
         return goods;
     }
 
+    @Override
     public List<GoodsEntity> findAll() {
         TypedQuery<GoodsEntity> query = em.createQuery("select g from GoodsEntity g", GoodsEntity.class);
         List<GoodsEntity> goodsList = query.getResultList();
         return goodsList;
     }
 
+    @Override
     public void update(GoodsEntity goods) {
         em.merge(goods);
     }
 
+    @Override
     public void delete(GoodsEntity goods) {
         em.remove(goods);
     }
--- src/main/java/repository/impl/OrdersListRepositoryImpl.java
+++ src/main/java/repository/impl/OrdersListRepositoryImpl.java
@@ -15,25 +15,30 @@ public class OrdersListRepositoryImpl implements OrdersListRepository {
     @PersistenceContext(name = "myUnit")
     private EntityManager em;
 
+    @Override
     public void create(OrdersListEntity ordersList) {
         em.persist(ordersList);
     }
 
+    @Override
     public OrdersListEntity read(int id) {
         OrdersListEntity ordersList = em.find(OrdersListEntity.class, id);
         return ordersList;
     }
 
+    @Override
     public List<OrdersListEntity> findAll() {
         TypedQuery<OrdersListEntity> query = em.createQuery("select o from OrdersListEntity o", OrdersListEntity.class);
         List<OrdersListEntity> ordersList = query.getResultList();
         return ordersList;
     }
 
+    @Override
     public void update(OrdersListEntity ordersList) {
         em.merge(ordersList);
     }
 
+    @Override
     public void delete(OrdersListEntity ordersList) {
         em.remove(ordersList);
     }
