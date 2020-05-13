4db5a06bb387ff4eca6a364647f8df9475b2126a
Fedor Zholud <fvzholud@gmail.com>
2020-05-13 13:24:36 +0300

    add soft delete for order

--- src/main/java/entity/impl/OrderEntity.java
+++ src/main/java/entity/impl/OrderEntity.java
@@ -3,12 +3,14 @@ package entity.impl;
 
 
 import entity.JpaBaseEntity;
+import org.hibernate.annotations.ResultCheckStyle;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import javax.persistence.Entity;
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +19,8 @@ import java.util.List;
  */
 
 @Entity
+@SQLDelete(sql = "UPDATE orders SET state = 'DELETED' where order_number = ?", check = ResultCheckStyle.COUNT)
+@Where(clause = "state <> 'DELETED'")
 @NamedQuery(name = "entity.impl.OrderEntity.findAll", query = "select o from OrderEntity o")
 @Table(name = "orders")
 public class OrderEntity extends JpaBaseEntity implements Serializable {
@@ -31,6 +35,10 @@ public class OrderEntity extends JpaBaseEntity implements Serializable {
 
     @Column(name = "order_time")
     private Timestamp orderTime;
+
+    @Column(name = "state")
+    @Enumerated(EnumType.STRING)
+    private OrderState orderState;
 
     @OneToMany
     @JoinColumn(name = "order_number")
@@ -62,6 +70,19 @@ public class OrderEntity extends JpaBaseEntity implements Serializable {
 
     public List<OrderLineEntity> getOrdersListEntities() {
         return orderLineEntities;
+    }
+
+    public OrderState getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(OrderState orderState) {
+        this.orderState = orderState;
+    }
+
+    @PreRemove
+    public void deleteOrder() {
+        this.orderState = OrderState.DELETED;
     }
 
     @Override
